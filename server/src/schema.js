ac5e4f4770fa8511792166afdfc79d3c63c05c22
unicodeveloper <prosperotemuyiwa@gmail.com>
2018-09-27 14:58:34 +0100

    Add server schema

---
+++ server/src/schema.js
@@ -0,0 +1,46 @@
+const { gql } = require('apollo-server');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    launches(limit: Int, offset: Int): [Launch]!
+    launch(id: ID!): Launch
+    trips(user: ID): [Launch]!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrip(user: ID!, trip: ID!): Boolean!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(user: ID!, trip: ID!): Boolean!
+
+    login(email: String): String
+  }
+
+  type Launch {
+    id: ID!
+    year: String!
+    date: String!
+    mission: String
+    rocket: Rocket!
+    launch_success: Boolean
+    passengers: [User]!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String!
+    type: String!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    avatar: String
+    trips: [Launch]!
+  }
+`;
+
+module.exports = typeDefs;
