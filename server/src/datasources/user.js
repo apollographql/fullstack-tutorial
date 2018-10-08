a506fef3d57cf571ed3354774fb61b84a9b9540c
Jake <dawkinsjh@gmail.com>
2018-10-08 11:28:17 -0400

    added notes

--- server/src/datasources/user.js
+++ server/src/datasources/user.js
@@ -7,10 +7,21 @@ class UserAPI extends DataSource {
     this.store = store;
   }
 
+  /**
+   * This is a function that gets called by ApolloServer when being setup.
+   * This function gets called with the datasource config including things
+   * like caches and context. We'll assign this.context to the request context
+   * here, so we can know about the user making requests
+   */
   initialize(config) {
     this.context = config.context;
   }
 
+  /**
+   * User can be called with an argument that includes email, but it doersn't
+   * have to be. If the user is already on the context, it will use that user
+   * instead
+   */
   async findOrCreateUser({ email: emailArg } = {}) {
     const email =
       this.context && this.context.user ? this.context.user.email : emailArg;
