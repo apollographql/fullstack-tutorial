0f6a8d031f27cf330b7b32b02c16b19dfcc0d0b1
Jake <dawkinsjh@gmail.com>
2018-10-09 16:51:08 -0400

    basic styling & fixes to queries

--- client/src/components/launch-tile.js
+++ client/src/components/launch-tile.js
@@ -3,41 +3,101 @@ import styled from 'react-emotion';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const Container = styled('div')({
-  border: '1px solid black',
-  margin: '32px 0',
-});
-
 const BOOK_TRIP = gql`
   mutation book($launchId: ID!) {
     bookTrip(launchId: $launchId)
   }
 `;
 
-export default ({ launch: { id, mission, rocket, year }, isLoggedIn }) => {
+const CANCEL_TRIP = gql`
+  mutation cancel($launchId: ID!) {
+    cancelTrip(launchId: $launchId)
+  }
+`;
+
+export default ({
+  launch: { id, mission, rocket, year },
+  isLoggedIn,
+  isBooked,
+}) => {
   return (
     <Container>
       <img
         src={mission.missionPatch}
-        width="100px"
+        style={{ height: '100px' }}
         alt={`Mission patch for ${mission.name}`}
       />
-      <p>
-        <strong>Mission</strong>:{mission.name}, <em>{year}</em>
-      </p>
-      <p>
-        <strong>Rocket</strong>: {rocket.name}
-      </p>
+      <Content>
+        <Title>
+          <strong>Mission</strong>: {mission.name}, <em>{year}</em>
+        </Title>
+        <Description>
+          <strong>Rocket</strong>: {rocket.name}
+        </Description>
 
-      {isLoggedIn ? (
-        <Mutation mutation={BOOK_TRIP}>
-          {(book, { data }) => (
-            <button onClick={() => book({ variables: { launchId: id } })}>
-              Book Trip
-            </button>
-          )}
-        </Mutation>
-      ) : null}
+        {isLoggedIn ? (
+          <Mutation mutation={isBooked ? CANCEL_TRIP : BOOK_TRIP}>
+            {(book, { data }) => (
+              <BookButton
+                isBooked={isBooked}
+                onClick={() => book({ variables: { launchId: id } })}
+              >
+                {isBooked ? 'Cancel Trip' : 'Book Trip'}
+              </BookButton>
+            )}
+          </Mutation>
+        ) : null}
+      </Content>
     </Container>
   );
 };
+
+/**
+ * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
+ */
+
+const BookButton = styled('button')(({ isBooked }) => ({
+  backgroundColor: 'white',
+  border: isBooked ? '1px solid #eb193e' : '1px solid #00194b',
+  color: isBooked ? '#eb193e' : '#00194b',
+  borderRadius: '3px',
+  paddingTop: '8px',
+  paddingBottom: '8px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '14px',
+  marginTop: '16px',
+  ':hover': {
+    backgroundColor: isBooked ? '#eb193e' : '#00194b',
+    color: 'white',
+  },
+}));
+
+const Container = styled('div')({
+  border: '1px solid #ccc',
+  borderRadius: '3px',
+  // borderBottom: '2px solid #eb193e',
+  margin: '32px 0',
+  display: 'flex',
+  flexDirection: 'row',
+  padding: '16px',
+  boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+});
+
+const Title = styled('p')({
+  fontSize: '20px',
+  fontWeight: '400',
+  marginTop: 0,
+});
+
+const Description = styled('p')({
+  margin: 0,
+});
+
+const Content = styled('div')({
+  marginLeft: '16px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+});
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,27 +3,22 @@ import styled from 'react-emotion';
 
 import LaunchList from '../components/launch-list';
 
-const Container = styled('header')({
-  maxWidth: '720px',
-  margin: '0 auto',
-  width: '100%',
-  // height: '48px',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-});
-
-// const FullWidthContainer = styled('div')({
-//   backgroundColor: '#00194b',
-//   color: '#fff',
-//   margin: '0',
-//   width: '100%',
-//   display: 'flex',
-//   justifyContent: 'center',
-// });
-
 export default () => (
   <Container>
     <LaunchList />
   </Container>
 );
+
+/**
+ * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
+ */
+
+const Container = styled('div')({
+  maxWidth: '720px',
+  margin: '0 auto',
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '0 8px',
+});
