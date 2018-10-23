83e0421e4624d21b86013901f043701b8632151e
Peggy Rayzis <peggyrayzis@gmail.com>
2018-10-23 09:24:45 -0400

    Modified schema to reflect new designs

--- server/src/datasources/launch.js
+++ server/src/datasources/launch.js
@@ -11,18 +11,17 @@ class LaunchAPI extends RESTDataSource {
     return {
       id: launch.flight_number || 0,
       cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
       mission: {
         name: launch.mission_name,
         missionPatchSmall: launch.links.mission_patch_small,
         missionPatchLarge: launch.links.mission_patch,
       },
-      year: launch.launch_year,
       rocket: {
         id: launch.rocket.rocket_id,
         name: launch.rocket.rocket_name,
         type: launch.rocket.rocket_type,
       },
-      launchSuccess: launch.launch_success,
     };
   }
 
