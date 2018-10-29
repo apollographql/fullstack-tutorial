f0457ed943ec2161ac6e03ee1841ad6aeb9cbb0c
Trevor Blades <tdblades@gmail.com>
2018-10-29 16:02:15 -0700

    Share the PageContainer component more and modify the launch tile styles a bit

--- client/src/components/page-container.js
+++ client/src/components/page-container.js
@@ -1,11 +1,10 @@
 import styled from 'react-emotion';
 
 export default styled('div')({
-  maxWidth: '720px',
-  margin: '0 auto',
-  width: '100%',
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
+  maxWidth: 720,
+  margin: '0 auto',
   padding: '16px 8px',
 });
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import styled from 'react-emotion';
 
+import PageContainer from '../components/page-container';
 import Launches from '../containers/launches';
 
 export default () => (
-  <Container>
+  <PageContainer>
     <Launches />
-  </Container>
+  </PageContainer>
 );
-
-/**
- * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
- */
-
-const Container = styled('div')({
-  maxWidth: '720px',
-  margin: '0 auto',
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: '0 8px',
-});
