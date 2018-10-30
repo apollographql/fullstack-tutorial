2f3ae7ef56e77786972c776b3695a664bec6954e
Trevor Blades <tdblades@gmail.com>
2018-10-30 15:00:31 -0700

    Inject global styles with emotion, add source sans typeface

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,28 @@
+import { injectGlobal } from 'react-emotion';
+
 export const unit = 8;
-export const maxWidth = 720;
 export const colors = {
-  primary: '#00194b',
-  secondary: '#eb193e'
-}
+  primary: '#220a82',
+  secondary: '#14cbc4',
+  accent: '#e535ab',
+  background: '#f7f8fa',
+  text: '#343c5a',
+  textSecondary: '#747790'
+};
+
+export default () => injectGlobal({
+  [['html', 'body', '#root']]: {
+    height: '100%',
+  },
+  body: {
+    margin: 0,
+    padding: 0,
+    fontFamily: "'Source Sans Pro', sans-serif",
+    backgroundColor: colors.background,
+    color: colors.text,
+  },
+  '#root': {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+});
