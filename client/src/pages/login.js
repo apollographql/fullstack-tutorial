70245d3e93d217d523fd3a934bd834c1ef3fa365
Trevor Blades <tdblades@gmail.com>
2018-10-30 18:34:53 -0700

    Adjust input size

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -112,7 +112,7 @@ const StyledForm = styled('form')(flexColumnCenter, {
 const StyledInput = styled('input')({
   width: '100%',
   marginBottom: unit * 2,
-  padding: `${unit}px ${unit * 2.5}px`,
+  padding: `${unit * 1.25}px ${unit * 2.5}px`,
   border: `1px solid ${colors.grey}`,
   fontSize: 16,
   outline: 'none',
