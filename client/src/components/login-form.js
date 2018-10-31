3ab60fd480eaf1b92d5f6273551302da9a5db71b
Trevor Blades <tdblades@gmail.com>
2018-10-31 16:53:16 -0700

    Remove unused dep

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled, { css } from 'react-emotion';
+import styled from 'react-emotion';
 
 import Button from './button';
 import space from '../assets/space.png';
