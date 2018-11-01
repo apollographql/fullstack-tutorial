57cdc2ef23163eb14d150f471ecea736ea96ba66
Trevor Blades <tdblades@gmail.com>
2018-10-31 17:10:20 -0700

    Fix the routing focus issue

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,45 +1,25 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
-import styled from 'react-emotion';
 
 import Launch from './launch';
 import Launches from './launches';
 import Cart from './cart';
 import Profile from './profile';
 import Footer from '../components/footer';
-import { unit, colors } from '../styles';
+import PageContainer from '../components/page-container';
 
 export default function Pages() {
   return (
     <Fragment>
-      <Bar />
-      <Container>
-        <Router>
-          <Launches path="/" />
-          <Launch path="/launch/:launchId" />
-          <Cart path="/cart" />
-          <Profile path="/profile" />
-        </Router>
-      </Container>
+      <Router>
+        <PageContainer path="/">
+          <Launches default />
+          <Launch path="launch/:launchId" />
+          <Cart path="cart" />
+          <Profile path="profile" />
+        </PageContainer>
+      </Router>
       <Footer />
     </Fragment>
   );
 }
-
-/**
- * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
- */
-
-const Bar = styled('div')({
-  flexShrink: 0,
-  height: 12,
-  backgroundColor: colors.primary,
-});
-
-const Container = styled('div')({
-  width: '100%',
-  maxWidth: 600,
-  margin: '0 auto',
-  padding: unit * 3,
-  paddingBottom: unit * 5
-});
