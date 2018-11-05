d0e83ffc391f71a563196216a7052661ae8c76f0
Peggy Rayzis <peggyrayzis@gmail.com>
2018-11-05 20:48:12 +1100

    Remove isLoggedIn query from pages and containers

--- client/src/pages/launches.js
+++ client/src/pages/launches.js
@@ -22,12 +22,10 @@ export const LAUNCH_TILE_DATA = gql`
 
 const GET_LAUNCHES = gql`
   query launchList($after: String) {
-    isLoggedIn @client
     launches(after: $after) {
       cursor
       hasMore
       launches {
-        isInCart @client
         ...LaunchTile
       }
     }
@@ -47,39 +45,36 @@ export default function Launches() {
             <Header />
             {data.launches &&
               data.launches.launches &&
-              data.launches.launches.map((l, index) => (
-                <LaunchTile
-                  key={l.id}
-                  launch={l}
-                  isLoggedIn={data.isLoggedIn}
-                />
+              data.launches.launches.map(launch => (
+                <LaunchTile key={launch.id} launch={launch} />
               ))}
-            {data.launches && data.launches.hasMore ? (
-              <Button
-                onClick={() =>
-                  fetchMore({
-                    variables: {
-                      after: data.launches.cursor,
-                    },
-                    updateQuery: (prev, { fetchMoreResult, ...rest }) => {
-                      if (!fetchMoreResult) return prev;
-                      return {
-                        ...fetchMoreResult,
-                        launches: {
-                          ...fetchMoreResult.launches,
-                          launches: [
-                            ...prev.launches.launches,
-                            ...fetchMoreResult.launches.launches,
-                          ],
-                        },
-                      };
-                    },
-                  })
-                }
-              >
-                Load More
-              </Button>
-            ) : null}
+            {data.launches &&
+              data.launches.hasMore && (
+                <Button
+                  onClick={() =>
+                    fetchMore({
+                      variables: {
+                        after: data.launches.cursor,
+                      },
+                      updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                        if (!fetchMoreResult) return prev;
+                        return {
+                          ...fetchMoreResult,
+                          launches: {
+                            ...fetchMoreResult.launches,
+                            launches: [
+                              ...prev.launches.launches,
+                              ...fetchMoreResult.launches.launches,
+                            ],
+                          },
+                        };
+                      },
+                    })
+                  }
+                >
+                  Load More
+                </Button>
+              )}
           </Fragment>
         );
       }}
