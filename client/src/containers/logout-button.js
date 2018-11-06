fd3a68812c2b61c31fe0c06fc75f691ed1863f59
Peggy Rayzis <peggyrayzis@gmail.com>
2018-11-06 13:16:20 -0800

    Update Apollo Client
    Update Apollo Client

--- client/src/containers/logout-button.js
+++ client/src/containers/logout-button.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'react-emotion';
 import { ApolloConsumer } from 'react-apollo';
 
 import { menuItemClassName } from '../components/menu-item';
 import { ReactComponent as ExitIcon } from '../assets/icons/exit.svg';
 
-export default class LogoutButton extends Component {
-  logout = client => {
-    client.writeData({ data: { isLoggedIn: false } });
-    localStorage.clear();
-  };
-
-  render() {
-    return (
-      <ApolloConsumer>
-        {client => (
-          <StyledButton onClick={() => this.logout(client)}>
-            <ExitIcon />
-            Logout
-          </StyledButton>
-        )}
-      </ApolloConsumer>
-    );
-  }
+export default function LogoutButton() {
+  return (
+    <ApolloConsumer>
+      {client => (
+        <StyledButton
+          onClick={() => {
+            client.writeData({ data: { isLoggedIn: false } });
+            localStorage.clear();
+          }}
+        >
+          <ExitIcon />
+          Logout
+        </StyledButton>
+      )}
+    </ApolloConsumer>
+  );
 }
 
 /**
