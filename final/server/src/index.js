3764026e1736dc1f6f8724127f5aad4f1583ac28
Peggy Rayzis <peggyrayzis@gmail.com>
2018-11-08 12:07:07 -0800

    Move Engine internal demo

--- final/server/src/index.js
+++ final/server/src/index.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const {ApolloServer} = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const isEmail = require('isemail');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-const {createStore} = require('./utils');
+const { createStore } = require('./utils');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
+
+const internalEngineDemo = require('./engine-demo');
 
 // creates a sequelize connection once. NOT for every request
 const store = createStore();
@@ -16,22 +18,22 @@ const store = createStore();
 // set up any dataSources our resolvers need
 const dataSources = () => ({
   launchAPI: new LaunchAPI(),
-  userAPI: new UserAPI({store}),
+  userAPI: new UserAPI({ store }),
 });
 
 // the function that sets up the global context for each resolver, using the req
-const context = async ({req}) => {
+const context = async ({ req }) => {
   // simple auth check on every request
   const auth = (req.headers && req.headers.authorization) || '';
   const email = new Buffer(auth, 'base64').toString('ascii');
 
   // if the email isn't formatted validly, return null for user
-  if (!isEmail.validate(email)) return {user: null};
+  if (!isEmail.validate(email)) return { user: null };
   // find a user by their email
-  const users = await store.users.findOrCreate({where: {email}});
+  const users = await store.users.findOrCreate({ where: { email } });
   const user = users && users[0] ? users[0] : null;
 
-  return {user: {...user.dataValues}};
+  return { user: { ...user.dataValues } };
 };
 
 // Set up Apollo Server
@@ -42,13 +44,7 @@ const server = new ApolloServer({
   context,
   engine: {
     apiKey: process.env.ENGINE_API_KEY,
-    schemaTag: 'current',
-    generateClientInfo: ({request}) => {
-      if (!request || !request.http) return {};
-      const clientName = request.http.headers.get('client-name');
-      const clientVersion = request.http.headers.get('client-version');
-      return {clientName, clientVersion};
-    },
+    ...internalEngineDemo,
   },
 });
 
@@ -56,8 +52,8 @@ const server = new ApolloServer({
 // if we're in a test env, we'll manually start it in a test
 if (process.env.NODE_ENV !== 'test')
   server
-    .listen({port: 4000})
-    .then(({url}) => console.log(`🚀 app running at ${url}`));
+    .listen({ port: 4000 })
+    .then(({ url }) => console.log(`🚀 app running at ${url}`));
 
 // export all the important pieces for integration/e2e tests to use
 module.exports = {
