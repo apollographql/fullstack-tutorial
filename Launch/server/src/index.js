31a6724793c45834de9f9bba54e68128ef8454fe
Kyle-Neale <kyle.a.neale@gmail.com>
2018-11-21 14:26:22 -0500

    Added mutations for login, bookTrips, and cancelTrip. Added minimal User authentication as well.

--- Launch/server/src/index.js
+++ Launch/server/src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
 const { createStore } = require('./utils');
 
 const typeDefs = require('./schema');
@@ -10,6 +11,19 @@ const UserAPI = require('./datasources/user');
 const store = createStore();
 
 const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = new Buffer(auth, 'base64').toString('ascii');
+
+    // if the email isn't formatted validly, return null for user
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
+
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
@@ -17,6 +31,7 @@ const server = new ApolloServer({
     userAPI: new UserAPI({ store }),
   })
 });
+
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
--- Launch/server/src/resolvers.js
+++ Launch/server/src/resolvers.js
@@ -52,4 +52,45 @@ module.exports = {
       );
     },
   },
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) {
+        return new Buffer(email).toString('base64');
+      }
+    },
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+    const results = await dataSources.userAPI.bookTrips({ launchIds });
+    const launches = await dataSources.launchAPI.getLaunchesByIds({
+      launchIds,
+    });
+
+    return {
+      success: results && results.length === launchIds.length,
+      message:
+        results.length === launchIds.length
+          ? 'trips booked successfully'
+          : `the following launches couldn't be booked: ${launchIds.filter(
+              id => !results.includes(id),
+            )}`,
+      launches,
+    };
+  },
+  cancelTrip: async (_, { launchId }, { dataSources }) => {
+    const result = dataSources.userAPI.cancelTrip({ launchId });
+
+    if (!result)
+      return {
+        success: false,
+        message: 'failed to cancel trip',
+      };
+
+    const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch],
+      };
+    },
+  },
 };
