32710e0ad3482105bb718eb2fb16f720eac2db84
Kyle-Neale <kyle.a.neale@gmail.com>
2018-11-27 12:34:34 -0500

    Added some thangs

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -2,13 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom"
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import Pages from "./pages"
+import Launches from "./pages/launches"
 import gql from "graphql-tag";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000"
 });
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
-  </ApolloProvider>, document.getElementById('root'));
+    <Launches />
+  </ApolloProvider>, document.getElementById('root')
+);
--- start/client/src/pages/launches.js
+++ start/client/src/pages/launches.js
@@ -7,7 +7,7 @@ import Header from '../components/header';
 import Button from '../components/button';
 import Loading from '../components/loading';
 
-export const GET_LAUNCHES = gql`
+const GET_LAUNCHES = gql`
   query launchList($after: String) {
     launches(after: $after) {
       cursor
@@ -27,3 +27,47 @@ export const GET_LAUNCHES = gql`
     }
   }
 `;
+
+export default function Launches() {
+  return (
+    <Query query={GET_LAUNCHES}>
+      {({ data, loading, error, fetchMore }) => {
+        if (loading) return <Loading />;
+        if (error) return <p>ERROR</p>;
+        return (
+          <Fragment>
+            <Header />
+              {data.launches &&
+                data.launches.hasMore && (
+                  <Button
+                    onClick={() =>
+                      fetchMore({
+                        variables: {
+                          after: data.launches.cursor,
+                        },
+                        updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                          if (!fetchMoreResult) return prev;
+                          return {
+                            ...fetchMoreResult,
+                            launches: {
+                              ...fetchMoreResult.launches,
+                              launches: [
+                                ...prev.launches.launches,
+                                ...fetchMoreResult.launches.launches,
+                              ],
+                            },
+                          };
+                        },
+                      })
+                    }
+                  >
+                    Load More
+                  </Button>
+                )
+              }
+          </Fragment>
+        );
+      }}
+    </Query>
+  );
+};
