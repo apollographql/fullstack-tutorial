5f9b05a38a5a5ec8a61e765f3bc2c9b6dcaaea99
Kyle-Neale <kyle.a.neale@gmail.com>
2018-11-27 10:06:25 -0500

    Updated package.json file with Apollo Boost and added imported Pages file.

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,5 +1,15 @@
-import { ApolloClient } from "apollo-client";
+import React from "react"
+import ReactDOM from "react-dom"
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Pages from "./pages"
+import gql from "graphql-tag";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: "http://localhost:4000"
 });
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>, document.getElementById('root'));
