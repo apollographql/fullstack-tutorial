c731dd5471dbcb97a5f4293202495ff6c6f63b81
Kyle-Neale <kyle.a.neale@gmail.com>
2018-11-27 11:51:26 -0500

    Added Apollo config file and added an Apollo query that fetches launches.

---
+++ start/client/src/pages/launches.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import LaunchTile from '../components/launch-tile';
+import Header from '../components/header';
+import Button from '../components/button';
+import Loading from '../components/loading';
+
+export const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`;
