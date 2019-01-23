8f3dbb43a1eefeaa43e8352dab318eb14031e42a
Borek Bernard <borekb@gmail.com>
2019-01-23 16:16:40 +0100

    Scaffold React components, revert pages/index.js

--- start/client/src/pages/index.js
+++ start/client/src/pages/index.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
 
+import Launch from './launch';
 import Launches from './launches';
-import { PageContainer } from '../components';
+import Cart from './cart';
+import Profile from './profile';
+import { Footer, PageContainer } from '../components';
 
 export default function Pages() {
   return (
@@ -10,8 +13,12 @@ export default function Pages() {
       <PageContainer>
         <Router primary={false} component={Fragment}>
           <Launches path="/" />
+          <Launch path="launch/:launchId" />
+          <Cart path="cart" />
+          <Profile path="profile" />
         </Router>
       </PageContainer>
+      <Footer />
     </Fragment>
   );
 }
