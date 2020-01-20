052a82bf172ff4671c001113db3fb326eada5979
hwillson <hugh@octonary.com>
2020-01-20 06:58:33 -0500

    Get rid of create-react-app's lint error about || &&

--- final/client/src/pages/cart.tsx
+++ final/client/src/pages/cart.tsx
@@ -25,7 +25,7 @@ const Cart: React.FC<CartProps> = () => {
   return (
     <Fragment>
       <Header>My Cart</Header>
-      {!data || !!data && data.cartItems.length === 0 ? (
+      {!data || (!!data && data.cartItems.length === 0) ? (
         <p data-testid="empty-message">No items in your cart</p>
       ) : (
         <Fragment>
