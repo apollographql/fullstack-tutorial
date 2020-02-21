79fce298ad406901822758c472bf9844b2e33148
hwillson <hugh@octonary.com>
2020-02-21 06:53:42 -0500

    Use makeLocalVar to track isLoggedIn instead of using the cache
    Stop writing/reading `isLoggedIn` directly from the cache, using
    a `isLoggedInVar` created `makeLocalVar` variable instead. Note
    that this commit introduces temporary redirects to update the
    app to properly show a logged in / logged out state. Eventually
    this won't be necessary as `makeLocalVar` changes will broadcast
    and trigger the re-running of queries automatically, meaning
    the logged in / logged out state will update automatically.

--- final/client/src/containers/logout-button.tsx
+++ final/client/src/containers/logout-button.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import styled from 'react-emotion';
-import { useApolloClient } from '@apollo/client';
 
 import { menuItemClassName } from '../components/menu-item';
+import { isLoggedInVar } from '../cache';
 import { ReactComponent as ExitIcon } from '../assets/icons/exit.svg';
 
 const LogoutButton = () => {
-  const client = useApolloClient();
   return (
     <StyledButton
       data-testid="logout-button"
       onClick={() => {
-        client.writeData({ data: { isLoggedIn: false } });
         localStorage.clear();
+        isLoggedInVar(false);
+
+        // TODO: This redirect is temporary. Eventually `makeLocalVar`
+        // (which is used to create `isLoggedInVar`) will broadcast changes
+        // which will result in the `IS_LOGGED_IN` query in `index.tsx`
+        // automatically re-running, and refreshing the app to show the login
+        // screen. For now though, this redirect will force the query to
+        // re-run and show the login.
+        window.location.href = '/';
       }}
     >
       <ExitIcon />
