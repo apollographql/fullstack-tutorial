5aac3c58b7fb64dbd8e3872a2a3e71b273c87afe
hwillson <hugh@octonary.com>
2020-02-22 07:11:11 -0500

    Remove soon to be unnecessary refetchQueries
    When `makeLocalVar` is updated to broadcast, `refetchQueries`
    will no longer be needed to refresh the cart. We'll use a
    temporary redirect to simulate the behavior for now.

--- final/client/src/containers/book-trips.tsx
+++ final/client/src/containers/book-trips.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { gql, useMutation } from '@apollo/client';
 
 import Button from '../components/button';
-import { GET_LAUNCH } from './cart-item';
 import { cartItemsVar } from '../cache';
 import * as GetCartItemsTypes from '../pages/__generated__/GetCartItems';
 import * as BookTripsTypes from './__generated__/BookTrips';
@@ -23,14 +22,13 @@ export const BOOK_TRIPS = gql`
 interface BookTripsProps extends GetCartItemsTypes.GetCartItems {}
 
 const BookTrips: React.FC<BookTripsProps> = ({ cartItems }) => {
-  const [bookTrips, { data }] = useMutation<BookTripsTypes.BookTrips, BookTripsTypes.BookTripsVariables>(
+  const [bookTrips, { data }] = useMutation<
+    BookTripsTypes.BookTrips,
+    BookTripsTypes.BookTripsVariables
+  >(
     BOOK_TRIPS,
     {
       variables: { launchIds: cartItems },
-      refetchQueries: cartItems.map(launchId => ({
-        query: GET_LAUNCH,
-        variables: { launchId },
-      })),
     }
   );
 
@@ -41,6 +39,14 @@ const BookTrips: React.FC<BookTripsProps> = ({ cartItems }) => {
         onClick={async () => {
           await bookTrips();
           cartItemsVar([]);
+
+          // TODO: This redirect is temporary. Eventually `makeLocalVar`
+          // (which is used to create `cartItemsVar`) will broadcast changes
+          // which will result in the `GET_CART_ITEMS` query in `cart.tsx`
+          // automatically re-running, and refreshing the app to show the
+          // cart is empty. For now though, this redirect will force the query
+          // to re-run and show the empty cart.
+          window.location.href = '/cart';
         }}
         data-testid="book-button"
       >
