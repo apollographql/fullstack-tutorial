782c91b9bd8afc7ec8f35f20c22cb5b6ff08f629
hwillson <hugh@octonary.com>
2020-02-22 13:34:52 -0500

    Test updates to accommodate new cache API changes

--- final/client/src/containers/__tests__/logout-button.tsx
+++ final/client/src/containers/__tests__/logout-button.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { InMemoryCache } from '@apollo/client';
 import LogoutButton from '../logout-button';
-import gql from 'graphql-tag';
 
 import { renderApollo, cleanup, fireEvent } from '../../test-utils';
-import { isLoggedInVar } from '../../cache';
+import { cache, isLoggedInVar } from '../../cache';
 
 describe('logout button', () => {
   // automatically unmount and cleanup DOM after the test is finished.
@@ -15,24 +13,13 @@ describe('logout button', () => {
   });
 
   it('complete logout', async () => {
-    const cache = new InMemoryCache();
     isLoggedInVar(true);
     localStorage.setItem('token', 'testTokenValue');
+    localStorage.setItem('userId', 'abc123');
     const { getByTestId } = renderApollo(<LogoutButton />, { cache });
-
     fireEvent.click(getByTestId('logout-button'));
-
-    // check to make sure the cache's contents have been updated
-    const response: any = cache.readQuery({
-      query: gql`
-        query IsUserLoggedIn {
-          isLoggedIn @client
-        }
-      `
-    });
-    const { isLoggedIn } = response;
-
-    expect(isLoggedIn).toBeFalsy();
+    expect(isLoggedInVar()).toBeFalsy();
     expect(localStorage.getItem('token')).toBeNull();
+    expect(localStorage.getItem('userId')).toBeNull();
   });
 });
