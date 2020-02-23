52c387478df3d74d609a352a7ff0518bcb2571c2
hwillson <hugh@octonary.com>
2020-02-23 05:35:03 -0500

    Replace the cache.modify deletion example with cache.evict
    This shows how `cache.evict()` works, and we're alreay showing
    how `cache.modify()` works in another part of the application.
    Also, using `evict` in this case is much more succinct.

--- final/client/src/containers/logout-button.tsx
+++ final/client/src/containers/logout-button.tsx
@@ -13,19 +13,12 @@ const LogoutButton = () => {
       data-testid="logout-button"
       onClick={() => {
         // Since we're logging out, remove all traces of the current user
-        // from the cache. First use `cache.modify()` to remove the stored
+        // from the cache. First use `cache.evict()` to remove the stored
         // `me` reference that was added to the cache by the `GET_MY_TRIPS`
         // query in `profile.tsx`. Then trigger garbage collection using
         // `cache.gc()` to remove the cached `User` object that is no longer
         // reachable.
-        client.cache.modify(
-          'ROOT_QUERY',
-          {
-            me(_, { DELETE }) {
-              return DELETE;
-            }
-          }
-        );
+        client.cache.evict('ROOT_QUERY', 'me');
         client.cache.gc();
 
         // Remove user details from localStorage.
