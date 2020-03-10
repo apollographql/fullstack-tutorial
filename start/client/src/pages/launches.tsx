384b2e7358e6c81d7351933d2a65ff4ff05f2e5b
thomasJs8 <thomas18thx@gmail.com>
2020-03-10 12:11:52 +0900

    fix an error at first npm start

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,11 +1,13 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import gql from "graphql-tag";
+import { RouteComponentProps } from "@reach/router";
+
+export const LAUNCH_TILE_DATA = gql``;
 
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
   return <div />;
-}
+};
 
 export default Launches;
-
