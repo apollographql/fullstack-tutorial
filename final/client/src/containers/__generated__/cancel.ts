a1c5d7f9621308261bdc7e4bbdcbe856088e3591
hwillson <hugh@octonary.com>
2020-06-13 10:06:46 -0400

    Update to latest @apollo/client RC and adjust tests to pass

--- final/client/src/containers/__generated__/cancel.ts
+++ final/client/src/containers/__generated__/cancel.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
+// @generated
 // This file was automatically generated and should not be edited.
 
 // ====================================================
