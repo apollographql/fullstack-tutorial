ac20c1fbcb02f3b6c31a87331b8445f033a180c7
Trevor Blades <tdblades@gmail.com>
2020-06-16 16:20:05 -0700

    Move trip counter to its own component

--- final/client/src/pages/index.tsx
+++ final/client/src/pages/index.tsx
@@ -5,52 +5,7 @@ import Launch from './launch';
 import Launches from './launches';
 import Cart from './cart';
 import Profile from './profile';
-import { Footer, PageContainer } from '../components';
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
-
-const TRIPS_QUERY = gql`
-  query TripsQuery {
-    tripsBooked
-  }
-`;
-
-const TRIPS_SUBSCRIPTION = gql`
-  subscription TripsSubscription {
-    tripsBooked
-  }
-`;
-
-function TripCounterInner(props: any) {
-  useEffect(() => props.subscribeToMore({
-    document: TRIPS_SUBSCRIPTION,
-    updateQuery: (prev: any, { subscriptionData }: { subscriptionData: any }) => ({
-      ...prev,
-      tripsBooked: prev.tripsBooked + subscriptionData.data.tripsBooked
-    })
-  }))
-
-  return <p>Trips booked: {props.tripsBooked}</p>
-}
-
-function TripCounter() {
-  const {data, loading, error, subscribeToMore} = useQuery(TRIPS_QUERY);
-
-  if (loading) {
-    return <p>Loading...</p>
-  }
-
-  if (error) {
-    return <p>{error.message}</p>
-  }
-
-  return (
-    <TripCounterInner
-      tripsBooked={data.tripsBooked}
-      subscribeToMore={subscribeToMore}
-    />
-  )
-}
+import { Footer, PageContainer, TripCounter } from '../components';
 
 export default function Pages() {
   return (
