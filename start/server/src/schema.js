9f16e13684a53be4a993fa2d9aeeb55952190142
Pegah Eizad <peizadkh@gmail.com>
2020-08-05 09:40:41 -0400

    schema constructed

---
+++ start/server/src/schema.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        launches: [Launch]!
+        launch(id: ID!): Launch
+        me: User
+    }
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): String # login token
+    }
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
+    type Launch {
+        id: ID!
+        site: String
+        mission: Mission
+        rocket: Rocket
+        isBooked: Boolean!
+    }
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+      }
+      
+      type User {
+        id: ID!
+        email: String!
+        trips: [Launch]!
+      }
+      
+      type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
+      }
+      
+      enum PatchSize {
+        SMALL
+        LARGE
+      }
+`;
+
+module.exports = typeDefs;
