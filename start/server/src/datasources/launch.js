8575cf1f1063143a0180b19295b8ca69bec1e3eb
Pegah Eizad <peizadkh@gmail.com>
2020-08-05 10:24:18 -0400

    connects dataSource

---
+++ start/server/src/datasources/launch.js
@@ -0,0 +1,46 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class LaunchAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://api.spacexdata.com/v2/';
+  }
+
+  async getAllLaunches() {
+    const response = await this.get('launches');
+    return Array.isArray(response)
+      ? response.map(launch => this.launchReducer(launch))
+      : [];
+  }
+
+  launchReducer(launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
+
+  async getLaunchById({ launchId }) {
+    const response = await this.get('launches', { flight_number: launchId });
+    return this.launchReducer(response[0]);
+  }
+  
+  getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map(launchId => this.getLaunchById({ launchId })),
+    );
+  }
+}
+
+module.exports = LaunchAPI;
--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,19 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({ 
+    typeDefs,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    }) 
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
