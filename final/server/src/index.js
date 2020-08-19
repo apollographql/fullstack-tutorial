56ef9e0ccc492ce2acb5a6cca1ded374ef940321
wzedi <warrick@orchestrated.io>
2020-08-19 23:28:46 +1000

    migrates apollo server to lambda

--- final/server/src/index.js
+++ final/server/src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-lambda');
 const isEmail = require('isemail');
 
 const typeDefs = require('./schema');
@@ -21,49 +21,36 @@ const dataSources = () => ({
   userAPI: new UserAPI({ store }),
 });
 
-// the function that sets up the global context for each resolver, using the req
-const context = async ({ req }) => {
-  // simple auth check on every request
-  const auth = (req.headers && req.headers.authorization) || '';
-  const email = new Buffer(auth, 'base64').toString('ascii');
-
-  // if the email isn't formatted validly, return null for user
-  if (!isEmail.validate(email)) return { user: null };
-  // find a user by their email
-  const users = await store.users.findOrCreate({ where: { email } });
-  const user = users && users[0] ? users[0] : null;
-
-  return { user };
-};
-
 // Set up Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources,
-  context,
   introspection: true,
   playground: true,
   engine: {
     apiKey: process.env.ENGINE_API_KEY,
     ...internalEngineDemo,
   },
+  context: async ({ event, context }) => {
+    const auth = (event.headers && event.headers.authorization) || '';
+    const email = new Buffer(auth, 'base64').toString('ascii');
+
+    // if the email isn't formatted validly, return null for user
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
+
+    return { user };
+  }
 });
 
-// Start our server if we're not in a test env.
-// if we're in a test env, we'll manually start it in a test
-if (process.env.NODE_ENV !== 'test') {
-  server
-    .listen({ port: process.env.PORT || 4000 })
-    .then(({ url }) => {
-      console.log(`🚀 app running at ${url}`)
-    });
-}
+const graphqlHandler = server.createHandler();
 
 // export all the important pieces for integration/e2e tests to use
 module.exports = {
   dataSources,
-  context,
   typeDefs,
   resolvers,
   ApolloServer,
@@ -71,4 +58,5 @@ module.exports = {
   UserAPI,
   store,
   server,
+  graphqlHandler,
 };
