8abc5c05304009f2e80a599731355262dbcd8aaf
Saad Aslam <saadusufzai@gmail.com>
2020-09-22 19:14:44 +0500

    Update resolvers.js

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -5,6 +5,51 @@ module.exports = {
       launch: (_, { id }, { dataSources }) =>
         dataSources.launchAPI.getLaunchById({ launchId: id }),
       me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
-    }
+    },
+    Mission: {
+        // The default size is 'LARGE' if not provided
+        missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+          return size === 'SMALL'
+            ? mission.missionPatchSmall
+            : mission.missionPatchLarge;
+        },
+      },
+      Launch: {
+  isBooked: async (launch, _, { dataSources }) =>
+    dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+},
+User: {
+  trips: async (_, __, { dataSources }) => {
+    // get ids of launches by user
+    const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+    if (!launchIds.length) return [];
+    // look up those launches by their ids
+    return (
+      dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      }) || []
+    );
+  },
+},Launch: {
+    isBooked: async (launch, _, { dataSources }) =>
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchIds.length) return [];
+      // look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      );
+    },
+  },
+
+
+  
   };
+  
   
