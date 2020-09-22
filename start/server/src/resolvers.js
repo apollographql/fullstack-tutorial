966feea01bf6fba13657524047728b4405051b56
Saad Aslam <saadusufzai@gmail.com>
2020-09-22 18:47:22 +0500

    Update resolvers.js

---
+++ start/server/src/resolvers.js
@@ -0,0 +1,10 @@
+module.exports = {
+    Query: {
+      launches: (_, __, { dataSources }) =>
+        dataSources.launchAPI.getAllLaunches(),
+      launch: (_, { id }, { dataSources }) =>
+        dataSources.launchAPI.getLaunchById({ launchId: id }),
+      me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+    }
+  };
+  
