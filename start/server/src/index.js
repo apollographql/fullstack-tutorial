1720ad195cd6b1f209b3d566973c37872b274a66
Saad Aslam <saadusufzai@gmail.com>
2020-09-22 16:35:33 +0500

    Update index.js

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,4 +2,4 @@ require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs })
