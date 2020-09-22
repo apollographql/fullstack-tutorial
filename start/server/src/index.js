10fa667c1f74bbd03dec82e25e189f1a438ce1e7
Saad Aslam <saadusufzai@gmail.com>
2020-09-22 18:03:33 +0500

    Update index.js

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils')
 
-const server = new ApolloServer({ typeDefs })
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({ 
+    typeDefs,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+      })
+})
 
 server.listen().then(({ url })=>{
     console.log(`🚀 Server is ready at ${url}`)
