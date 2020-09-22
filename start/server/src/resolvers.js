4a88d9c80821cbd1d91ce2b43a40725fcd2d0a46
Saad Aslam <saadusufzai@gmail.com>
2020-09-22 19:28:59 +0500

    Update resolvers.js

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,7 +1,27 @@
+const { paginateResults } = require('./utils')
+
 module.exports = {
     Query: {
-      launches: (_, __, { dataSources }) =>
-        dataSources.launchAPI.getAllLaunches(),
+        launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+            const allLaunches = await dataSources.launchAPI.getAllLaunches();
+            // we want these in reverse chronological order
+            allLaunches.reverse();
+            const launches = paginateResults({
+              after,
+              pageSize,
+              results: allLaunches
+            });
+            return {
+              launches,
+              cursor: launches.length ? launches[launches.length - 1].cursor : null,
+              // if the cursor at the end of the paginated results is the same as the
+              // last item in _all_ results, then there are no more results after this
+              hasMore: launches.length
+                ? launches[launches.length - 1].cursor !==
+                  allLaunches[allLaunches.length - 1].cursor
+                : false
+            };
+          },
       launch: (_, { id }, { dataSources }) =>
         dataSources.launchAPI.getLaunchById({ launchId: id }),
       me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
@@ -49,7 +69,7 @@ User: {
   },
 
 
-  
+
   };
   
   
