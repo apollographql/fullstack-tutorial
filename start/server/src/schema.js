815ccb64aa535050ef093955621ae67a65078647
Saad Aslam <saadusufzai@gmail.com>
2020-09-22 19:22:02 +0500

    Update schema.js

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -30,10 +30,26 @@ const typeDefs = gql`
   }
 
   type Query {
-    launches: [Launch]!
+    launches(
+        """
+        The number of results to show. Must be >= 1. Default = 20
+        """
+        pageSize: Int
+        """
+        If you add a cursor here, it will only return results _after_ this cursor
+        """
+        after: String
+      ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
   }
+
+  type LaunchConnection { 
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
