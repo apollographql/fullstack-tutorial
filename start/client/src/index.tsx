ec204ed03102680ba5dcd9815dd2d5cde95dd31b
Saad Aslam <saadusufzai@gmail.com>
2020-09-23 20:34:56 +0500

    Update index.tsx

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,12 +1,26 @@
 import {
-    ApolloClient,
-    InMemoryCache,
-    gql,
-    NormalizedCacheObject
-  } from '@apollo/client';
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  NormalizedCacheObject,
+} from "@apollo/client";
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
   
-  const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-    uri: 'http://localhost:4000/',
-    cache: new InMemoryCache()
-  });
-  
+});
+client
+  .query({
+    query: gql`
+      query TestQuery {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
