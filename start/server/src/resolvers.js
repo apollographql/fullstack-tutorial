2acadf39b3e7b618270f21c8f8513fae20c158e8
Saad Aslam <saadusufzai@gmail.com>
2020-09-23 11:06:26 +0500

    Update resolvers.js

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -30,7 +30,40 @@ module.exports = {
     login: async (_, { email }, { dataSources }) => {
       const user = await dataSources.userAPI.findOrCreateUser({ email });
       if (user) return Buffer.from(email).toString('base64');
-    }
+    },
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+  
+      return {
+        success: results && results.length === launchIds.length,
+        message:
+          results.length === launchIds.length
+            ? 'trips booked successfully'
+            : `the following launches couldn't be booked: ${launchIds.filter(
+                id => !results.includes(id),
+              )}`,
+        launches,
+      };
+    },
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+  
+      if (!result)
+        return {
+          success: false,
+          message: 'failed to cancel trip',
+        };
+  
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch],
+      };
+    },
   },
 
   Mission: {
