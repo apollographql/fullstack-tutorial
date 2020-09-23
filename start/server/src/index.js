712d7bf2ee0806a8ed34b0196bce27871c9f3977
Saad Aslam <saadusufzai@gmail.com>
2020-09-23 10:58:58 +0500

    Update index.js

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,24 +1,35 @@
-require('dotenv').config();
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
 
-const { createStore } = require('./utils')
-const resolvers = require(`./resolvers`)
+const { createStore } = require("./utils");
+const resolvers = require(`./resolvers`);
 
-const LaunchAPI = require('./datasources/launch');
-const UserAPI = require('./datasources/user');
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+const isEmail = require("isemail");
 
 const store = createStore();
 
-const server = new ApolloServer({ 
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-        launchAPI: new LaunchAPI(),
-        userAPI: new UserAPI({ store })
-      })
-})
+const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+    return { user: { ...user.dataValues } };
+  },
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
-server.listen().then(({ url })=>{
-    console.log(`🚀 Server is ready at ${url}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server is ready at ${url}`);
+});
