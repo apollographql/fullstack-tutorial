a980a5a7fdc568af61664e4b8e9c97c862680584
Saad Aslam <saadusufzai@gmail.com>
2020-09-23 20:40:05 +0500

    Update index.tsx

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,26 +1,24 @@
 import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  NormalizedCacheObject,
-} from "@apollo/client";
-
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  uri: "http://localhost:4000/",
-  cache: new InMemoryCache(),
+    ApolloClient,
+    InMemoryCache,
+    NormalizedCacheObject,
+    ApolloProvider
+  } from '@apollo/client';
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import Pages from './pages';
+  import injectStyles from './styles';
   
-});
-client
-  .query({
-    query: gql`
-      query TestQuery {
-        launch(id: 56) {
-          id
-          mission {
-            name
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+  const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache()
+  });
+  
+  injectStyles();
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <Pages />
+    </ApolloProvider>,
+    document.getElementById('root')
+  );
+  
