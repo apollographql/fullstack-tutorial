e3ca5c8aea2c9deeecf323765964eb3d28d59620
Tyler Becks <tylerdbecks@gmail.com>
2020-09-25 14:24:40 -0700

    Connect to data sources
    This is step 2/9

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,21 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({ typeDefs });
+const { createStore } = require('./utils');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
