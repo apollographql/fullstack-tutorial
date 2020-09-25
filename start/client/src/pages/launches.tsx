becab5671e4ca93dbb39c7e06792dd99562daa7f
Saad Aslam <saadusufzai@gmail.com>
2020-09-25 13:18:16 +0500

    Update launches.tsx

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,9 +1,9 @@
-import React, { Fragment } from 'react';
-import { gql, useQuery } from '@apollo/client';
+import React, { Fragment } from "react";
+import { gql, useQuery } from "@apollo/client";
 
-import { LaunchTile, Header, Button, Loading } from '../components';
-import { RouteComponentProps } from '@reach/router';
-import * as GetLaunchListTypes from './__generated__/GetLaunchList';
+import { LaunchTile, Header, Button, Loading } from "../components";
+import { RouteComponentProps } from "@reach/router";
+import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
 
 const GET_LAUNCHES = gql`
   query launchList($after: String) {
@@ -28,11 +28,7 @@ const GET_LAUNCHES = gql`
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  const {
-    data,
-    loading,
-    error
-  } = useQuery<
+  const { data, loading, error, fetchMore } = useQuery<
     GetLaunchListTypes.GetLaunchList,
     GetLaunchListTypes.GetLaunchListVariables
   >(GET_LAUNCHES);
@@ -49,8 +45,36 @@ const Launches: React.FC<LaunchesProps> = () => {
         data.launches.launches.map((launch: any) => (
           <LaunchTile key={launch.id} launch={launch} />
         ))}
+        
+      {data.launches && data.launches.hasMore && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                after: data.launches.cursor,
+              },
+
+              updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                if (!fetchMoreResult) return prev;
+                return {
+                  ...fetchMoreResult,
+                  launches: {
+                    ...fetchMoreResult.launches,
+                    launches: [
+                      ...prev.launches.launches,
+                      ...fetchMoreResult.launches.launches,
+                    ],
+                  },
+                };
+              },
+            })
+          }
+        >
+          Load More
+        </Button>
+      )}
     </Fragment>
   );
-}
+};
 
 export default Launches;
