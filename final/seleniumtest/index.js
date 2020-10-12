478db9c1a386fac2c2e742e3525771c59b96a752
Matt Rommel <rommelmatt@gmail.com>
2020-10-12 14:48:20 -0400

    Types in your email

--- final/seleniumtest/index.js
+++ final/seleniumtest/index.js
@@ -1,5 +1,22 @@
 const webdriver = require("selenium-webdriver");
+const { elementIsNotSelected } = require("selenium-webdriver/lib/until");
 const driver = new webdriver.Builder().forBrowser("chrome").build();
+const By = webdriver.By;
 
-driver.navigate().to("http://google.com/");
+function inputEmail(element, email){
+    element.sendKeys('Email@site.com')
+}
+function findLoginElement(email){
+    driver.findElement(By.name('email')).then(element => inputEmail(element, email), function(err){ console.log(err)});
+}
+function Login(email="address@server.com"){
+    element = findLoginElement(email);
+}
+//Driver into the Client
+driver
+    .navigate()
+    .to("http://localhost:3000")
+    .then(() => Login())
+    .catch(error => console.log(error))
+
 console.log("In Selenium Test")
