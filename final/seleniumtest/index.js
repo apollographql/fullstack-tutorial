4774098591d79e5d217b242b513afbbce9adcfd6
Matt Rommel <rommelmatt@gmail.com>
2020-10-12 20:14:38 -0400

    Full Login Sequence

--- final/seleniumtest/index.js
+++ final/seleniumtest/index.js
@@ -3,11 +3,21 @@ const { elementIsNotSelected } = require("selenium-webdriver/lib/until");
 const driver = new webdriver.Builder().forBrowser("chrome").build();
 const By = webdriver.By;
 
+
+function LoginButton(){
+    driver.findElement(By.xpath('/html/body/div/div/form/button')).then(element => 
+        element.click(), 
+        function(err){ console.log(err)});
+}
 function inputEmail(element, email){
-    element.sendKeys('Email@site.com')
+    element.sendKeys('Email@site.com').then(()=>
+    LoginButton(),
+    function(err){ console.log(err)});
 }
 function findLoginElement(email){
-    driver.findElement(By.name('email')).then(element => inputEmail(element, email), function(err){ console.log(err)});
+    driver.findElement(By.name('email')).then(element => 
+        inputEmail(element, email), 
+        function(err){ console.log(err)});
 }
 function Login(email="address@server.com"){
     element = findLoginElement(email);
