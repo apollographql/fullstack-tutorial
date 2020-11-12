d176a850b7a17ab7d0a68293394dfe7b437b86c2
Matt Rommel <rommelmatt@gmail.com>
2020-11-12 11:46:39 -0500

    Selenium wrapper class with programmatic commands. Next step: integrate into testing

--- final/seleniumtest/index.js
+++ final/seleniumtest/index.js
@@ -2,12 +2,12 @@ const webdriver = require("selenium-webdriver");
 const { elementIsNotSelected } = require("selenium-webdriver/lib/until");
 const driver = new webdriver.Builder().forBrowser("chrome").build();
 const By = webdriver.By;
+const sleeptime = 3000; //how long to wait after each command to make tests visually percievable 
 
-const sleeptime = 1000; //how long to wait after each command to make tests visually percievable 
 
-async function clickWhenClickable(locator,timeout=2000){
-    driver.wait(function(){
-      return driver.findElement(locator).then(function(element){        
+async function clickWhenClickable(locator,timeout=2000, webdriver=driver){
+    webdriver.wait(function(){
+      return webdriver.findElement(locator).then(function(element){        
         return element.click().then(function(){
           return true;
         }, function(err){
@@ -19,9 +19,9 @@ async function clickWhenClickable(locator,timeout=2000){
     }, timeout, 'Timeout waiting for ' + locator.value);    ;
   }
 
- async function sendKeysWhenSendable(locator,keys, timeout=2000){
-    driver.wait(function(){
-      return driver.findElement(locator).then(function(element){        
+ async function sendKeysWhenSendable(locator,keys, timeout=2000, webdriver=driver){
+    webdriver.wait(function(){
+      return webdriver.findElement(locator).then(function(element){        
         return element.sendKeys(keys).then(async function(){
           return true;
         }, function(err){
@@ -32,42 +32,81 @@ async function clickWhenClickable(locator,timeout=2000){
       });
     }, timeout, 'Timeout waiting for ' + locator.value);    ;
   }
-// function LogInLogOutTest(driver){
-//     try{
-//         driver.navigate().to("http://localhost:3000");
-//         driver.sleep(sleeptime).then(()=> {
-//             sendKeysWhenSendable(By.name('email'),"Email@site.com" ).then(()=>{ 
-//                 driver.sleep(sleeptime).then(()=> {
-//                     clickWhenClickable(By.xpath('/html/body/div/div/form/button'), 10*1000).then(()=>{
-//                         driver.sleep(sleeptime).then(()=> {
-//                             clickWhenClickable(By.xpath("//*[contains(@data-testid, 'logout')]"), 10*1000); 
-//                         }); 
-//                     }); 
-//                 });
-//             });
-//         });
-        
-//     }
-//     catch(e){
-//         console.log(e);
-//     }
+  class DriverWrapper{
+    constructor(driver){
+        this.driver=driver; 
+        this.ActionList=[];
+    }
+    navigate(url){
+        var that=this;
+        this.ActionList.push(
+            function() {that.driver.navigate().to("http://localhost:3000");}
+        );
+    }
+    click(locator, timeout=2*1000){
+        var that=this;
+        this.ActionList.push(
+            function() {clickWhenClickable(locator, timeout);}
+        );
+    }
+    sendKeys(locator, keys, timeout=2*1000){
+        var that=this;
+        this.ActionList.push(
+            function() {sendKeysWhenSendable(locator, keys, timeout);}
+        );
+    }
+    async execute(){
+        while(this.ActionList.length>0) {
+            await this.ActionList.shift()();
+            await this.driver.sleep(sleeptime)
+        }
+    }
+}
 
-// }
 async function LogInLogOutTestAsync(driver){
     try{
         await driver.navigate().to("http://localhost:3000");
         await driver.sleep(sleeptime)
         await sendKeysWhenSendable(By.name('email'),"Email@site.com" )
         await driver.sleep(sleeptime)
-        await clickWhenClickable(By.xpath('/html/body/div/div/form/button'), 10*1000)
+        await clickWhenClickable(By.xpath('/html/body/div/div/form/button'), 2*1000)
         await driver.sleep(sleeptime)
-        await clickWhenClickable(By.xpath("//*[contains(@data-testid, 'logout')]"), 10*1000);      
+        await clickWhenClickable(By.xpath("//*[contains(@data-testid, 'logout')]"), 2*1000);      
     }
     catch(e){
         console.log(e);
     }
 
 }
-LogInLogOutTestAsync(driver);
-// LogInLogOutTest(driver);
+var driverTest = new DriverWrapper(driver);
+driverTest.navigate("http://localhost:3000");
+driverTest.sendKeys(By.name('email'),"Email@site.com");
+driverTest.click(By.xpath('/html/body/div/div/form/button'));
+driverTest.click(By.xpath("//*[contains(@data-testid, 'logout')]"));
+driverTest.execute();
 
+
+
+//Old way (for reference only)
+function LogInLogOutTest(driver){
+    try{
+        driver.navigate().to("http://localhost:3000");
+        driver.sleep(sleeptime).then(()=> {
+            sendKeysWhenSendable(By.name('email'),"Email@site.com" ).then(()=>{ 
+                driver.sleep(sleeptime).then(()=> {
+                    clickWhenClickable(By.xpath('/html/body/div/div/form/button'), 10*1000).then(()=>{
+                        driver.sleep(sleeptime).then(()=> {
+                            clickWhenClickable(By.xpath("//*[contains(@data-testid, 'logout')]"), 10*1000); 
+                        }); 
+                    }); 
+                });
+            });
+        });
+        
+    }
+    catch(e){
+        console.log(e);
+    }
+
+}
+// LogInLogOutTestAsync(driver);
