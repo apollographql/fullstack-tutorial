b9bdddc657d3d1dabb79785169eca235edcfbbfa
Matt Rommel <rommelmatt@gmail.com>
2020-11-11 22:55:31 -0500

    Finally solved formatting with await statements!!

--- final/seleniumtest/index.js
+++ final/seleniumtest/index.js
@@ -3,7 +3,7 @@ const { elementIsNotSelected } = require("selenium-webdriver/lib/until");
 const driver = new webdriver.Builder().forBrowser("chrome").build();
 const By = webdriver.By;
 
-const sleeptime = 3000; //how long to wait after each command to make tests visually percievable 
+const sleeptime = 1000; //how long to wait after each command to make tests visually percievable 
 
 async function clickWhenClickable(locator,timeout=2000){
     driver.wait(function(){
@@ -32,25 +32,42 @@ async function clickWhenClickable(locator,timeout=2000){
       });
     }, timeout, 'Timeout waiting for ' + locator.value);    ;
   }
-async function LogInLogOutTest(driver){
-    try{
-        driver.navigate().to("http://localhost:3000");
-        driver.sleep(sleeptime).then(()=> {
-            sendKeysWhenSendable(By.name('email'),"Email@site.com" ).then(()=>{ 
-                driver.sleep(sleeptime).then(()=> {
-                    clickWhenClickable(By.xpath('/html/body/div/div/form/button'), 10*1000).then(()=>{
-                        driver.sleep(sleeptime).then(()=> {
-                            clickWhenClickable(By.xpath("//*[contains(@data-testid, 'logout')]"), 10*1000); 
-                        }); 
-                    }); 
-                });
-            });
-        });
+// function LogInLogOutTest(driver){
+//     try{
+//         driver.navigate().to("http://localhost:3000");
+//         driver.sleep(sleeptime).then(()=> {
+//             sendKeysWhenSendable(By.name('email'),"Email@site.com" ).then(()=>{ 
+//                 driver.sleep(sleeptime).then(()=> {
+//                     clickWhenClickable(By.xpath('/html/body/div/div/form/button'), 10*1000).then(()=>{
+//                         driver.sleep(sleeptime).then(()=> {
+//                             clickWhenClickable(By.xpath("//*[contains(@data-testid, 'logout')]"), 10*1000); 
+//                         }); 
+//                     }); 
+//                 });
+//             });
+//         });
         
+//     }
+//     catch(e){
+//         console.log(e);
+//     }
+
+// }
+async function LogInLogOutTestAsync(driver){
+    try{
+        await driver.navigate().to("http://localhost:3000");
+        await driver.sleep(sleeptime)
+        await sendKeysWhenSendable(By.name('email'),"Email@site.com" )
+        await driver.sleep(sleeptime)
+        await clickWhenClickable(By.xpath('/html/body/div/div/form/button'), 10*1000)
+        await driver.sleep(sleeptime)
+        await clickWhenClickable(By.xpath("//*[contains(@data-testid, 'logout')]"), 10*1000);      
     }
     catch(e){
         console.log(e);
     }
 
 }
-LogInLogOutTest(driver);
+LogInLogOutTestAsync(driver);
+// LogInLogOutTest(driver);
+
