3aff8e83cb935b9cf37f04b42a5457267fa6429c
Pauline Tannos <ptannos2@gmail.com>
2021-06-26 08:20:44 -0400

    server side is good, client side not good

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,27 +1,91 @@
-import React, { Fragment, useState }  from 'react';
-import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import React, { Fragment, useState } from "react";
+import { RouteComponentProps } from "@reach/router";
+import { gql, useQuery } from "@apollo/client";
+import { ArtworkTile, Header, Button, Loading } from "../components";
+import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
 
-export const LAUNCH_TILE_DATA = gql`
-  fragment LaunchTile on Launch {
-    __typename
-    id
-    isBooked
-    rocket {
-      id
-      name
-    }
-    mission {
-      name
-      missionPatch
+export const GET_ARTWORKS = gql`
+  query getArtworks {
+    artworks {
+      hasMore
+      artworks {
+        ...ArtworkTile
+      }
     }
   }
 `;
 
-interface LaunchesProps extends RouteComponentProps {}
+interface ArtworksProps extends RouteComponentProps {}
 
-const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
+const Artworks: React.FC<ArtworksProps> = () => {
+  const { data, loading, error } = useQuery<
+    GetArtworkListTypes.GetArtworkList,
+    GetArtworkListTypes.GetArtworkListVariables
+  >(GET_ARTWORKS);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <ArtworkTile key={launch.id} launch={launch} />
+        ))}
+    </Fragment>
+  );
+};
 
 export default Launches;
+
+// export const ARTWORK_TILE_DATA = gql`
+//   fragment ArtworkTile on Artwork {
+//     __typename
+//     id
+//     title
+//   }
+// `;
+
+// export const GET_ARTWORKS = gql`
+//   query getArtworks {
+//     artworks {
+//       hasMore
+//       artworks {
+//         ...ArtworkTile
+//       }
+//     }
+//   }
+// `;
+
+// interface ArtworksProps extends RouteComponentProps {}
+
+// const Artworks: React.FC<ArtworksProps> = () => {
+//   const {
+//     data,
+//     loading,
+//     error
+//   } = useQuery<
+//     GetArtworkListTypes.GetArtworkList,
+//     GetArtworkListTypes.GetArtworkListVariables
+//   >(GET_ARTWORKS);
+
+//   if (loading) return <Loading />;
+//   if (error) return <p>ERROR</p>;
+//   if (!data) return <p>Not found</p>;
+
+//   return (
+//     <Fragment>
+//       <Header />
+//       {data.launches &&
+//         data.launches.launches &&
+//         data.launches.launches.map((launch: any) => (
+//           <ArtworkTile key={launch.id} launch={launch} />
+//         ))}
+//     </Fragment>
+//   );
+// }
+
+// export default Launches;
