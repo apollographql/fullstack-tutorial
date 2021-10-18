0bb05a94ad9fe14b85d7c6f61041317e8cc66ea5
Delveri M Chick <delverimchick@wakki.lan>
2021-10-18 15:07:39 -0400

    Update menu item test

--- final/client/src/components/__tests__/menu-item.tsx
+++ final/client/src/components/__tests__/menu-item.tsx
@@ -4,15 +4,10 @@ import { shallow } from 'enzyme';
 
 import { render, cleanup } from '../../test-utils';
 import MenuItem from '../menu-item';
-import Button from "../button";
 
 describe('Menu Item', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
-
-  it('renders without error', () => {
-    render(<MenuItem to="/wow" />);
-  });
 
   it('renders Menu Item without error',  () => {
     const wrapper = shallow((
