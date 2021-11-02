12fbc1cc2c86cf05b4aa70c527d2f88ca15a9f63
arbaird <austinrbaird@mac.com>
2021-11-01 19:58:08 -0700

    Update action-button.tsx
    updated most tests for action-button to use enzyme

--- final/client/src/containers/__tests__/action-button.tsx
+++ final/client/src/containers/__tests__/action-button.tsx
@@ -3,27 +3,42 @@ import React from 'react';
 import { renderApollo, cleanup } from '../../test-utils';
 import ActionButton from '../action-button';
 import { cartItemsVar } from '../../cache';
+import { shallow, configure, mount, render  } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() })
 
 describe('action button', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
   it('renders without error', () => {
-    const { getByTestId } = renderApollo(<ActionButton />);
-    expect(getByTestId('action-button')).toBeTruthy();
+    const wrapper = shallow(<ActionButton />);
+    expect(wrapper.find({ "data-testid": "action-button" })).toBeTruthy();
+    //---replaced code for reference---
+    //const { getByTestId } = renderApollo(<ActionButton />);
+    //---end reference---
   });
 
-  it('shows correct label', () => {
-    const { getByText, container } = renderApollo(<ActionButton />);
-    getByText(/add to cart/i);
+  it('shows correct label', () => {   
+    let wrapper = shallow(<ActionButton />);
+    expect(wrapper.render().text().includes('Add to Cart')).toBe(true);
+    //---replaced code for reference---
+    //getByText(/add to cart/i);
+    //---end reference---
 
     // rerender with different props to same container
     cartItemsVar(['1']);
-    renderApollo(<ActionButton id="1" />, { container });
-    getByText(/remove from cart/i);
-    cartItemsVar([]);
+    wrapper = shallow(<ActionButton id="1" />);
+    expect(wrapper.render().text().includes('Remove from Cart')).toBe(true);
+    //---replaced code for reference---
+    //renderApollo(<ActionButton id="1" />, { container });
+    //getByText(/remove from cart/i);\
+    //---end reference---
 
     // rerender with different props to same container
+    cartItemsVar([]);
+    const { getByText, container } = renderApollo(<ActionButton />);
     renderApollo(<ActionButton isBooked={true} />, { container });
     getByText(/cancel this trip/i);
   });
