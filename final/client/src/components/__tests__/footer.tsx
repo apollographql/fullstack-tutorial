19945a56cb66cd2c6fdcc245501f159b8924eda1
Delveri M Chick <delverimchick@wakki.lan>
2021-11-15 21:44:24 -0500

    Fix footer and header unit tests

--- final/client/src/components/__tests__/footer.tsx
+++ final/client/src/components/__tests__/footer.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { renderApollo, cleanup } from '../../test-utils';
 import Footer from '../footer';
+import {MenuItem} from "../index";
+import {shallow} from "enzyme";
 
 describe('Footer', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
   it('renders without error', () => {
-    renderApollo(<Footer />);
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find(MenuItem)).toHaveLength(3);
   });
 });
--- final/client/src/components/__tests__/header.tsx
+++ final/client/src/components/__tests__/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { render, cleanup } from '../../test-utils';
 import Header from '../header';
+import {shallow} from "enzyme";
 
 describe('Header', () => {
   // automatically unmount and cleanup DOM after the test is finished.
@@ -9,5 +10,7 @@ describe('Header', () => {
 
   it('renders without error', () => {
     render(<Header />);
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('h2').text()).toBe('Space Explorer');
   });
 });
