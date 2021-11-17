26324eb26ee8fc979bfa40dc4dc957e0e8bdb973
bsheikh <bsheikh@uci.edu>
2021-11-16 19:04:55 -0800

    6/25 test cases updated

--- final/client/src/components/__tests__/button.tsx
+++ final/client/src/components/__tests__/button.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-
-import { render, cleanup } from '../../test-utils';
+import { configure, shallow, mount, render } from 'enzyme';
+import { cleanup } from '../../test-utils';
 import Button from '../button';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Button', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
   it('renders without error', () => {
-    render(<Button>Hello World</Button>);
+    const buttonObj1 = shallow(<Button>Shallow Button</Button>);
+    const buttonObj2 = mount(<Button>Full Button</Button>);
+    //console.log(buttonObj1.debug());
+    // console.log(shallowObj.get(0));
+    expect(buttonObj1.contains('Shallow Button')).toBe(true);
+    expect(buttonObj1.find('button').text()).toEqual("Shallow Button");
+
+    expect(buttonObj2.contains('Full Button')).toBe(true);
+    expect(buttonObj2.find('button').text()).toEqual("Full Button");
+
   });
 });
--- final/client/src/components/__tests__/loading.tsx
+++ final/client/src/components/__tests__/loading.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-
-import { render, cleanup } from '../../test-utils';
+import { configure, shallow, mount, render } from 'enzyme';
+import { cleanup } from '../../test-utils';
 import Loading from '../loading';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Loading', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
   it('renders without error', () => {
-    render(<Loading />);
+    const shallowObj = shallow(<Loading />);
+    expect(shallowObj.prop('className')).toBe('css-kahotv');
   });
 });
