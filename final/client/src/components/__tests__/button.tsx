7d47fde42b2098aaae19e76fec07de02780f9dbc
Corbin Beebe <corbinbeebe@gmail.com>
2021-11-18 18:25:35 -0800

    Button and loading unit tests
    Added unit test for the button and loading components

--- final/client/src/components/__tests__/button.tsx
+++ final/client/src/components/__tests__/button.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 import { render, cleanup } from '../../test-utils';
 import Button from '../button';
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 //new imports
 
-describe('Button', () => {
-  // automatically unmount and cleanup DOM after the test is finished.
-  afterEach(cleanup);
+configure({adapter: new Adapter})
 
-  it('renders without error', () => {
-    render(<Button>Hello World</Button>);
+describe('Button', () => {
+  it('makes sure the button does not blow up', () => {
+    const wrapper = shallow(<button />);
+    expect(wrapper.exists())
   });
+
+  // // automatically unmount and cleanup DOM after the test is finished.
+  // afterEach(cleanup);
+  //
+  // it('renders without error', () => {
+  //   render(<Button>Hello World</Button>);
+  // });
 });
--- final/client/src/components/__tests__/loading.tsx
+++ final/client/src/components/__tests__/loading.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 
 import { render, cleanup } from '../../test-utils';
 import Loading from '../loading';
-import {configure} from "enzyme";
+import {configure, shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({adapter: new Adapter()})
 
 describe('Loading', () => {
-
-
-
+  it('makes sure this loading component doesnt blow up', () => {
+    const wrapper = shallow(<Loading />);
+    expect(wrapper.exists());
+  });
 
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
-  it('renders without error', () => {
-    render(<Loading />);
-  });
 });
