2477bacb8c467b535cded9aba64ada06078d57d5
cindyquil <cindyn1995@gmail.com>
2021-11-29 23:42:02 -0800

    Completed last unit test
    Updated profile.tsx

--- final/client/src/pages/__tests__/profile.tsx
+++ final/client/src/pages/__tests__/profile.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import {
-  renderApollo,
   cleanup,
   waitForElement,
 } from '../../test-utils';
 import Profile, { GET_MY_TRIPS } from '../profile';
-import { mount, render } from '../../enzyme';
+import { mount } from '../../enzyme';
+import { act } from 'react-dom/test-utils';
+import { MockedProvider } from '@apollo/client/testing';
 
 const mockLaunch = {
   __typename: 'Launch',
@@ -25,28 +26,62 @@ const mockLaunch = {
   },
 };
 
-const mockMe = {
-  __typename: 'User',
-  id: 1,
-  email: 'a@a.a',
-  trips: [mockLaunch],
+const updateWrapper = async (wrapper, time = 0) => {
+  await act(async () => {
+    await new Promise((res) => setTimeout(res, time));
+    await wrapper.update();
+  });
 };
 
 describe('Profile Page', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
-  it('renders profile page', async () => {
+  it('renders profile page without any trips booked', async () => {
+    const mockMe = {
+      __typename: 'User',
+      id: 1,
+      email: 'a@a.a',
+      trips: [],
+    };
     const mocks = [
       {
         request: { query: GET_MY_TRIPS },
         result: { data: { me: mockMe } },
       },
     ];
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+      <Profile  />
+     </MockedProvider>,
+    );
 
-    const { getByText } = render(<Profile />, { mocks });
+    // if the profile renders with no trips booked, the text will show
+    await updateWrapper(wrapper);
+    expect(wrapper.html()).toContain("You haven't booked any trips");
+  });
 
-    // if the profile renders, it will have the list of missions booked
-    await waitForElement(() => getByText(/test mission/i));
+  it('renders profile page with 1 trip booked', async () => {
+    const mockMe = {
+      __typename: 'User',
+      id: 1,
+      email: 'a@a.a',
+      trips: [mockLaunch],
+    };
+    const mocks = [
+      {
+        request: { query: GET_MY_TRIPS },
+        result: { data: { me: mockMe } },
+      },
+    ];
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+      <Profile  />
+     </MockedProvider>,
+    );
+
+    // if the profile renders with a trip booked, the text will not show
+    await updateWrapper(wrapper);
+    expect(wrapper.html()).not.toContain("You haven't booked any trips");
   });
 });
