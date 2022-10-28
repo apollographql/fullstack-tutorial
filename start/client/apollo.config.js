37c91852e47ac8316a28121dfe2923ac3aa0e96a
John P. Vajda <jvajda@apollographql.com>
2022-10-28 17:41:52 -0600

    a few more changes

--- start/client/apollo.config.js
+++ start/client/apollo.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  client: {
-    name: "Full Stack Tutorial",
-    // url: "https://apollo-fullstack-tutorial.herokuapp.com/graphql",
-    service: "Full-Stack-Tutorial-ztag5s",
-  },
-};
+//empty
--- start/client/src/cache.ts
+++ start/client/src/cache.ts
@@ -1,3 +1,3 @@
-import { InMemoryCache } from '@apollo/client';
+import { InMemoryCache, Reference } from '@apollo/client';
 
 export const cache: InMemoryCache = new InMemoryCache({});
